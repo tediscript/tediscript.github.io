f407a04e938c801ee2be0ec5a8ad00e1da7763b7
tediscript <tediscript@gmail.com>
2020-07-09 21:09:12 +0700

    add vibrantjs

---
+++ vibrantjs/script.js
@@ -0,0 +1,68 @@
+(function () {
+    'use strict';
+    
+//    var loadFile = function(event) {
+//        var reader = new FileReader();
+//        reader.onload = function(){
+//          var output = document.getElementById('img');
+//          output.src = reader.result;
+//        };
+//        reader.readAsDataURL(event.target.files[0]);
+//      };
+    
+    var file = document.querySelector('#file');
+    file.addEventListener('change', function(event) {
+        var reader = new FileReader();
+        reader.onload = function(){
+          var output = document.getElementById('img');
+          output.src = reader.result;
+        };
+        reader.readAsDataURL(event.target.files[0]);
+    });
+    
+    var img = document.querySelector('#img'),
+        list = document.querySelector('ul'),
+        section = document.querySelector('section'),
+        paletteReady = false;
+        
+    var btn = document.querySelector('#load');
+    img.addEventListener('load', function() {
+//        if ( !paletteReady )
+            getPalette();
+    });
+    
+//    if (!paletteReady)
+//        getPalette();
+    
+    function getPalette() {
+        paletteReady = true;
+        
+        var vibrant = new Vibrant(img),
+            swatches = vibrant.swatches(),
+            listFragment = new DocumentFragment();
+        
+        for ( var swatch in swatches ) {
+            if (swatches.hasOwnProperty(swatch) && swatches[swatch]) { 
+                console.log(swatch, swatches[swatch].getHex());
+                var li = document.createElement('li'),
+                    p = document.createElement('p'),
+                    small = document.createElement('small');
+                
+                p.textContent = swatches[swatch].getHex();
+                p.style.color = swatches[swatch].getTitleTextColor();
+                small.textContent = swatch;
+                small.style.color = swatches[swatch].getBodyTextColor();
+                li.style.backgroundColor = swatches[swatch].getHex();
+                li.appendChild(p);
+                li.appendChild(small);
+                listFragment.appendChild(li);
+            }
+        }
+        list.innerHTML = '';
+        list.appendChild(listFragment);
+        
+        if (swatches['DarkVibrant']) {
+            section.style.backgroundColor = swatches['DarkVibrant'].getHex();
+        }
+    }
+} ());
